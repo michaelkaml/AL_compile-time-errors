f172e2d00895c733507c1e9a56afe46521bb5398
srijal30 <srijal30@stuy.edu>
2021-12-15 23:15:01 -0500

    for fun changes

--- ALTester.java
+++ ALTester.java
@@ -6,19 +6,24 @@ public class ALTester{
     //Normal Array List
     ArrayList<Integer> arrTheFuchs = new ArrayList<Integer>();
 
-    //Ordered Array List
+    //Ordered Array List (For Binary)
     OrderedArrayList michaelKaml = new OrderedArrayList();
+
+    //Ordered Array List (For Linear)
+    OrderedArrayList srijal30 = new OrderedArrayList();
+
 
     //Add the same randomly generated numbers
     for(int i = 0; i < 23; i++){
       int randomNum = (int) (Math.random() * 100);
       arrTheFuchs.add(randomNum);
       michaelKaml.addLinear(randomNum);
+      srijal30.addBinary(randomNum);
     }
 
 
     //Check if ArrayList is Sorted
-    boolean isSorted = false;
+    boolean isSorted = true;
     for( int i = 1; i < arrTheFuchs.size(); i++){
       isSorted = arrTheFuchs.get(i) >= arrTheFuchs.get(i-1);
       if(!isSorted) break;
@@ -27,13 +32,17 @@ public class ALTester{
     System.out.println( "ArrayList:\n" + arrTheFuchs + "\nSorted = " + isSorted); 
 
     //Check if OrderedArrayList is Sorted
-    isSorted = false;
+    isSorted = true;
+    boolean isSorted2 = true;
+
     for( int i = 1; i < michaelKaml.size(); i++){
-      isSorted = michaelKaml.get(i) >= michaelKaml.get(i-1);
+      isSorted = isSorted && michaelKaml.get(i) >= michaelKaml.get(i-1);
+      isSorted2 = isSorted2 && srijal30.get(i) >= srijal30.get(i-1);
       if(!isSorted) break;
     }
-
-    System.out.println( "\nOrderedArrayList:\n" + michaelKaml + "\nSorted = " + isSorted); 
+    
+    System.out.println( "\nOrderedArrayList (BIN):\n" + michaelKaml + "\nSorted = " + isSorted); 
+    System.out.println( "\nOrderedArrayList (LIN):\n" + srijal30 + "\nSorted = " + isSorted2); 
 
 
   }
